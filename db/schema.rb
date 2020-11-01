f753070af5a1bab41adb3234004b4379fc2e7bfe
saqla <whitecross8fortune8fatedress@gmail.com>
2020-11-01 12:42:38 +0900

    annotate

--- db/schema.rb
+++ db/schema.rb
@@ -21,4 +21,5 @@ ActiveRecord::Schema.define(version: 2020_10_24_113831) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
+
 end
